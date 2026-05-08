2a769d9d06a52db8a5f07a42e135c9d327027bca
Madhav Joshi <joshimadhav093@gmail.com>
2026-05-08 16:54:17 +0530

    update pom and test cases

--- src/main/java/pages/Sauce_login.java
+++ src/main/java/pages/Sauce_login.java
@@ -1,7 +1,35 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Sauce_login {
+WebDriver driver;
+     public  Sauce_login(WebDriver driver){
+         this.driver=driver;
+         PageFactory.initElements(driver,this);
+     }
 
+    @FindBy(xpath ="//input[@name='user-name']")
+    private WebElement user_name;
+    @FindBy(xpath ="//input[@name='password']")
+    private WebElement pass;
+    @FindBy(xpath="//input[@id='login-button']")
+    private WebElement login_bttn;
 
+    public void enter_user(String username){
+
+        user_name.sendKeys(username);
+    }
+
+    public void enter_pass(String password){
+        pass.sendKeys(password);
+    }
+
+    public void click_lbttn (){
+        login_bttn.click();
+    }
 
 }
---
+++ src/test/java/stepDefinitions/Step_login.java
@@ -0,0 +1,42 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.Sauce_login;
+
+public class Step_login {
+    WebDriver driver;
+    Sauce_login sl;
+    @Given("Browser is open")
+    public void launch(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+    }
+    @And("user is on login page")
+    public void homepage (){
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+    }
+    @Then("^User Enter (.*) and (.*)$")
+    public void enter_user_pass(String username,String password){
+       sl = new Sauce_login(driver);
+        sl.enter_user(username);
+        sl.enter_pass(password);
+    }
+    @When("click on login button")
+    public void click_btn (){
+        sl.click_lbttn();
+    }
+
+    @Then("user land on home page")
+    public void valid_Homepage(){
+
+    }
+
+}
